ac0629285c0b523432e90f7fd21553afb2725900
Asiyenur Yılmaz <asiyenurylmz17@gmail.com>
2019-10-24 00:28:24 +0300

    lambda expressions tutorials

--- src/main/java/com/java8/tutorial/functionalinterface/Main.java
+++ src/main/java/com/java8/tutorial/functionalinterface/Main.java
@@ -4,5 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		Foo foo = (x, y) -> (2 * x + y);
 		System.out.println(foo.apply(3, 4));
+
+		Bar bar = new Bar();
+		System.out.println(bar.calculate((x, y) -> (2 * x + y), 5, 4));
 	}
 }
